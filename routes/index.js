74bc1806c3c1fd6a8e76d380aa464ba0bd6bcd46
Ken Duigraha Putra <email": null>
2016-10-19 16:44:38 +0700

    add beforeCreate

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 let models = require('../models')
 let Urls = models.Url
-
+let sequelize = require('sequelize')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Shorten URL' });
 });
 
 let random_url = () => {
-  let result = 'short.id/'
+  let result = ''
   result += String.fromCharCode(Math.floor(Math.random()*9)+49)
   result += String.fromCharCode(Math.floor(Math.random()*26)+97) || String.fromCharCode(Math.floor(Math.random()*26)+65)
   result += String.fromCharCode(Math.floor(Math.random()*26)+65) || String.fromCharCode(Math.floor(Math.random()*26)+97)
@@ -18,23 +18,21 @@ let random_url = () => {
 }
 
 router.post('/urls', (req, res,next) => {
-  // console.log(req.body.input_url);
-  // console.log(random_url());
   let input_url = req.body.input_url
-  // console.log(/.([a-zA-Z]{2,3})$/.test(input_url));
-  //else if(/.com$/.test(input_url) === false){
-  //   res.render('index', {title: 'Shorten URL', error: "Input must link"})
-  // }
   if(input_url === ''){
     res.render('index', {title: 'Shorten URL', error: "Input must be filled"})
   }else{
-    Urls.create({
-      urls: input_url,
-      short_url: random_url(),
-    }).then(() => {
-      console.log(`Insert Data Success`);
+    Urls.beforeCreate(((url) => {
+      url.short_url = random_url()
+      return sequelize.Promise.resolve(url)
+    }))
 
-      res.redirect('/')
+    Urls.create({
+      urls: input_url
+    }).then((data) => {
+      console.log(`Insert Data Success`);
+      console.log(data.short_url)
+      res.redirect(`/${data.short_url}`)
     }).catch((err) => {
       res.render('index', {title: 'Shorten URL', error: "Input must link"})
     })
@@ -42,7 +40,7 @@ router.post('/urls', (req, res,next) => {
 })
 
 router.get('/:short_urls', (req, res, next) => {
-
+  res.redirect('/')
 })
 
 module.exports = router;
